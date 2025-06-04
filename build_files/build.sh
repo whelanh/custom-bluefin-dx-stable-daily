b51aef9404ed1172b89ca87f8f46ee777af70197
whelanh <brickhousedevelopers@gmail.com>
2025-06-04 12:20:26 -0400

    Giving up on Cosmic for now

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y @cosmic-desktop-environment tmux kmymoney micro python3-pip r fish fontawesome-fonts-all freetype-devel fribidi-devel zsh \
+dnf5 install -y tmux kmymoney micro python3-pip r fish fontawesome-fonts-all freetype-devel fribidi-devel zsh \
 	 java-latest-openjdk-devel gcc gcc-c++ clang llvm sqlitebrowser lftp libcurl-devel libjpeg-turbo-devel libpng-devel libtiff-devel \
 	 llvm-devel meson micro onedrive sqlitebrowser tcl8-devel tk8-devel typescript
   
